dbedf653fc467f2b4475975a0015b4aae1dcf0da
hsinyi204 <ian20000204@gmail.com>
2020-12-31 17:22:47 +0800

    1

--- RISCV.v
+++ RISCV.v
@@ -10,7 +10,14 @@ module RISCV(clk,
             // for mem_I
             mem_addr_I,
             mem_rdata_I);
+    input         clk, rst_n        ;
+    output [31:2] mem_addr_I        ;
+    input  [31:0] mem_rdata_I       ;
+    output        mem_wen_D         ;
+    output [31:2] mem_addr_D        ;
+    output [63:0] mem_wdata_D       ;
+    input  [63:0] mem_rdata_D       ;
 
-input clk, rst_n;
+
 
 endmodule
